4b110b2d00007eb70caed27ba306419a4b79cbd4
Mekarosi <mekarosi_assa@yahoo.com>
2020-07-30 19:28:59 +0100

    Create backend routes

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
-
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Contact keeper API...' })
 );
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Live on PORT ${PORT}`);

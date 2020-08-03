e0a7ba16663738c3804a8eafc72753a688c67fe6
Mekarosi <mekarosi_assa@yahoo.com>
2020-08-03 09:21:58 +0100

    Add navigation links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faIdCardAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -12,6 +13,14 @@ const Navbar = ({ title, icon }) => {
       <h1>
         <FontAwesomeIcon icon={['fas', 'id-card-alt']} /> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </div>
   );
 };

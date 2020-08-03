35e7b742fc86b748fe663837b71d1cb1e5d65cb8
Mekarosi <mekarosi_assa@yahoo.com>
2020-08-03 08:47:12 +0100

    Add Navbar and font awesome icon

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faIdCardAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faIdCardAlt);
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-success'>
+      <h1>
+        <FontAwesomeIcon icon={['fas', 'id-card-alt']} />
+        {title}
+      </h1>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt',
+};
+export default Navbar;

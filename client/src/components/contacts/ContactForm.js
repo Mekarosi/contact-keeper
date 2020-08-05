c5f6439ce779a1138170f45df9ca33463ab30481
Mekarosi <mekarosi_assa@yahoo.com>
2020-08-05 09:10:19 +0100

    Implement event handler on contactform

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
-  const [contact, setState] = useState({
+  const contactContext = useContext(ContactContext);
+
+  const [contact, setContact] = useState({
     name: '',
     email: '',
     phone: '',
@@ -10,8 +13,23 @@ const ContactForm = () => {
 
   const { name, email, phone, type } = contact;
 
+  const onChange = (e) => {
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    contactContext.addContact(contact);
+    setContact({
+      name: '',
+      email: '',
+      phone: '',
+      type: 'personal',
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2 className='text-primary'>Add Contact</h2>
       <input
         type='text'

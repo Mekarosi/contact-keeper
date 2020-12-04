0b6371d91cf5837f06a6df89098307fc22e05bc5
Mekarosi <mekarosi_assa@yahoo.com>
2020-12-04 20:25:52 +0100

    Authenticate user registeration on frontend

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import axios from 'axios'
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 import {
@@ -9,7 +10,8 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     LOGOUT,
-    CLEAR_ERRORS
+    CLEAR_ERRORS,
+    CLEAR_CURRENT
 } from '../types';
 
 const AuthState = (props) => {
@@ -24,14 +26,44 @@ const AuthState = (props) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load User
+  const loadUser = () => {
+    console.log('loadUser')
+  }
 
   // Register 
+  const register = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    try {
+      const res = await axios.post('/api/users', formData, config)
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      })
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg
+      })
+    }
+  }
 
   // Login User
+  const login = () => {
+    console.log('login')
+  }
 
   // Logout
+  const logout = () => {
+    console.log('logout')
+  }
 
   // Clear Errors
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS })
   
 
   return (
@@ -41,7 +73,12 @@ const AuthState = (props) => {
         isAuthenticated: state.isAuthenticated,
         loading: state.loading,
         user: state.user,
-        error: state.error
+        error: state.error,
+        register,
+        loadUser,
+        login,
+        logout,
+        clearErrors
       }}
     >
       {props.children}
